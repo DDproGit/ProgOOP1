29bf09353ee252113199a9c1bfc9947eaa9f3942
DDproGit <liran.capone@gmail.com>
2022-04-15 00:19:13 +0700

    Испрвление - забытые скобки

--- ProgOOP1/Parallelepiped.h
+++ ProgOOP1/Parallelepiped.h
@@ -5,7 +5,7 @@ class Parallelepiped :public Shape
 	int a, b, c;
 public:
 	Parallelepiped() { a = -1; b = -1; c = -1; };
-	~Parallelepiped();
+	~Parallelepiped() {};
 	void InData(ifstream& ifst);
 	void Out(ofstream& ofst);
 };
